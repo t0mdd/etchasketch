099e82dd876e480886dbe7b3284ab189db8f3d03
t0mdd <tomdd@protonmail.com>
2022-05-31 21:28:59 +0100

    Add eventListeners to change colour of gridSquares
    The eventListeners add a new class to the element when the mouse
    enters. This class styles the element with a new background color.

--- etchasketch.js
+++ etchasketch.js
@@ -1,13 +1,15 @@
 const container = document.querySelector('.container');
 
-for(let i = 1; i <= 4; i++){
+for(let i = 1; i <= 40; i++){
   let rowContainerDiv = document.createElement('div');
   rowContainerDiv.classList = 'rowContainer';
   rowContainerDiv.id = `rowContainer${i}`;
-  for(let j = 1; j <= 4; j++){
+  for(let j = 1; j <= 40; j++){
     let gridSquareDiv = document.createElement('div');
     gridSquareDiv.classList = 'gridSquare';
     gridSquareDiv.id = `gridSquare(${j},${i})`;
+    gridSquareDiv.addEventListener('mouseenter', 
+      () => gridSquareDiv.classList.add('gridHover'));
     rowContainerDiv.appendChild(gridSquareDiv);
   }
   container.appendChild(rowContainerDiv);
