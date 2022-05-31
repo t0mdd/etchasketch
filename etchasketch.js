7fe2448d0933c50736affd90233979fa8429f82d
t0mdd <tomdd@protonmail.com>
2022-05-31 21:59:58 +0100

    Added button to change grid dimensions
    Wrote a javascript function that changes the grid dimensions to the
    numbers passed as arguments if they are integers between 1 and
    100. Added an eventlistener to trigger this function when the button
    labelled change dimensions is clicked.

--- etchasketch.js
+++ etchasketch.js
@@ -1,16 +1,46 @@
 const container = document.querySelector('.container');
+const changeDimensionsBtn = 
+      document.querySelector('.changeDimensions');
 
-for(let i = 1; i <= 40; i++){
-  let rowContainerDiv = document.createElement('div');
-  rowContainerDiv.classList = 'rowContainer';
-  rowContainerDiv.id = `rowContainer${i}`;
-  for(let j = 1; j <= 40; j++){
-    let gridSquareDiv = document.createElement('div');
-    gridSquareDiv.classList = 'gridSquare';
-    gridSquareDiv.id = `gridSquare(${j},${i})`;
-    gridSquareDiv.addEventListener('mouseenter', 
-      () => gridSquareDiv.classList.add('gridHover'));
-    rowContainerDiv.appendChild(gridSquareDiv);
+changeDimensionsBtn.addEventListener('click',changeDimensions);
+
+function addGridSquares(noRows,noCols){
+  for(let i = 1; i <= noCols; i++){
+    let rowContainerDiv = document.createElement('div');
+    rowContainerDiv.classList = 'rowContainer';
+    rowContainerDiv.id = `rowContainer${i}`;
+    for(let j = 1; j <= noRows; j++){
+      let gridSquareDiv = document.createElement('div');
+      gridSquareDiv.classList = 'gridSquare';
+      gridSquareDiv.id = `gridSquare(${j},${i})`;
+      gridSquareDiv.addEventListener('mouseenter', 
+        () => gridSquareDiv.classList.add('gridHover'));
+      rowContainerDiv.appendChild(gridSquareDiv);
+    }
+    container.appendChild(rowContainerDiv);
   }
-  container.appendChild(rowContainerDiv);
 }
+
+function isInteger(num){
+  return typeof num === 'number' && num === Math.floor(num);
+}
+
+function validDimension(dim){
+  return isInteger(dim) && dim > 0 && dim <= 100;
+}
+
+function changeDimensions(){
+  let noRows, noCols;
+  while(!validDimension(noRows)){
+    noRows = +prompt('Enter the number of rows (Must be an integer ' +
+                     'between 1 and 100): ');
+  }
+  while(!validDimension(noCols)){
+    noCols = +prompt('Enter the number of columns (Must be an ' +
+                     'integer between 1 and 100): ');
+  }
+  container.innerHTML = ''; //remove its content
+  addGridSquares(noRows,noCols);
+}
+
+addGridSquares(40,40);
